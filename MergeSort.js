961b5da9e19f25755851525c07813e147342e5eb
Ben Finley <bben10hi@gmail.com>
2022-08-07 23:18:30 -0400

    finished mergeSort

--- MergeSort.js
+++ MergeSort.js
@@ -1,35 +1,75 @@
-const mergSort = (array) => {
-  if (array.length <= 1) {
-    return [array[0]];
+const mergeSort = (array) => {
+  console.log(array);
+  if (array.length === 0) {
+    return array;
   }
+  if (array.length === 1) {
+    return array;
+  }
+
   else {
-    const splitLeft = Math.round(array.length / 2);
-    const splitRight = array.length - splitLeft;
-    const leftHalf = array.slice(0, splitLeft);
-    const rightHalf = array.slice(splitRight, array.length);
-    const leftSorted = mergSort(leftHalf);
-    const rightSorted = mergSort(rightHalf);
-    const returnArray = []
-    for (i = 0; i <= leftSorted.length; i++) {
-      if (leftSorted[i] < rightSorted[i]) {
-        returnArray.push(leftSorted[i]);
-        returnArray.push(rightSorted[i]);
+    // split array in half
+    const split = Math.round(array.length / 2);
+    const leftHalf = array.slice(0, split);
+    const rightHalf = array.slice(split, array.length);
+    const leftSorted = mergeSort(leftHalf);
+    const rightSorted = mergeSort(rightHalf);
+
+    console.log(leftSorted)
+    console.log(rightSorted)
+
+    //sort array
+    const sortedArray = [];
+    while (leftSorted.length > 0 || rightSorted.length > 0) {
+      if (!leftSorted[0]) {
+        sortedArray.push(rightSorted.shift());
+      }
+      else if (!rightSorted[0]) {
+        sortedArray.push(leftSorted.shift());
+      }
+      else if (leftSorted[0] < rightSorted[0]) {
+        sortedArray.push(leftSorted.shift());
       }
       else {
-        returnArray.push(rightSorted[i]);
-        returnArray.push(leftSorted[i]);
+        sortedArray.push(rightSorted.shift());
       }
     }
-    return returnArray;
-  //   if (leftSorted < rightSorted) {
-  //     return [...leftSorted, ...rightSorted]
-  //   }
-  //   else {
-  //     return [...rightSorted, ...leftSorted]
-  //   }
+    return sortedArray
   }
 }
 
-const testArray = [5,2,7,1,8,3,6,4];
+const testArray = [1,5,2,7,1,8,3,6,4];
+const smallArray= [3,1,2];
+console.log(mergeSort(smallArray));
+console.log(mergeSort(testArray));
 
-console.log(mergSort(testArray));
+// const test = (leftSorted, rightSorted) => {
+//   const sortedArray = [];
+//   while (leftSorted.length > 0 || rightSorted.length > 0) {
+//     if (!leftSorted[0]) {
+//       sortedArray.push(rightSorted[0]);
+//       rightSorted.shift();
+//     }
+//     else if (!rightSorted[0]) {
+//       sortedArray.push(leftSorted[0]);
+//       rightSorted.shift();
+//     }
+//     else if (leftSorted[0] < rightSorted[0]) {
+//       sortedArray.push(leftSorted[0]);
+//       leftSorted.shift();
+//     }
+//     else {
+//       sortedArray.push(rightSorted[0]);
+//       rightSorted.shift();
+//     }
+//   }
+//   return sortedArray;
+// }
+
+// console.log(test(smallArray, testArray));
+
+// const testFunc = (array) => {
+//   array.shift();
+//   return array
+// }
+// console.log(testFunc(smallArray))
